c59400ebc17e3a7c5a6c887455e16bda8f1aa20b
kieran.schubert <schubert.kieran@gmail.com>
2024-11-28 16:57:22 +0100

    update ANCHOR regex

--- src/app/shared/utils/zco-utils.ts
+++ src/app/shared/utils/zco-utils.ts
@@ -1,4 +1,4 @@
-export const ANCHOR_TAG_REGEX = /<a\s+href=['"]([^'"]+)['"][^>]*>(.*?)<\/a>/gi;
+export const ANCHOR_TAG_REGEX = /<source><a\s+href=['"]([^'"]+)['"][^>]*>(.*?)<\/a><\/source>/gi;
 export const MESSAGE_ID_REGEX = /<message_uuid>(.*)<\/message_uuid>/gi;
 
 // Remove null, undefined and empty string value from object
