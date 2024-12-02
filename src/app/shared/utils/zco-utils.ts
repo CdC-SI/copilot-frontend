cbbe44991253ee3396fa9023a413609250900bc5
kieran.schubert <schubert.kieran@gmail.com>
2024-12-02 14:42:38 +0100

    update status message regex

--- src/app/shared/utils/zco-utils.ts
+++ src/app/shared/utils/zco-utils.ts
@@ -1,10 +1,10 @@
 export const ANCHOR_TAG_REGEX = /<source><a\s+href=['"]([^'"]+)['"][^>]*>(.*?)<\/a><\/source>/gi;
 export const MESSAGE_ID_REGEX = /<message_uuid>(.*)<\/message_uuid>/gi;
-export const RETRIEVING_TAG_REGEX = /<retrieving>(.*?)<\/retrieving>/gi;
+export const RETRIEVING_TAG_REGEX = /<retrieval>(.*?)<\/retrieval>/gi;
 export const TOPIC_CHECK_REGEX = /<topic_check>(.*?)<\/topic_check>/gi;
 export const OFF_TOPIC_REGEX = /<off_topic>(.*?)<\/off_topic>/gi;
 export const ROUTING_TAG_REGEX = /<routing>(.*?)<\/routing>/gi;
-export const AGENT_TAG_REGEX = /<agent>(.*?)<\/agent>/gi;
+export const AGENT_TAG_REGEX = /<agent_handoff>(.*?)<\/agent_handoff>/gi;
 
 // Remove null, undefined and empty string value from object
 export const clearNullAndEmpty = (obj: any): any => {
