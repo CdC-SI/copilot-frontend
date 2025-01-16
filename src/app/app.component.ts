ecc3495fedc6b678c702845e72ae29fa4b262ddc
kieran.schubert <schubert.kieran@gmail.com>
2025-01-16 16:45:52 +0100

    refactor user registration flow to handle login after successful account creation

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit} from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
 import {SignUpComponent} from './shared/components/sign-up/sign-up.component';
-import {UserService} from './shared/services/user.service';
+import {UserService, UserRegistrationResponse} from './shared/services/user.service';
 import {ObINavigationLink, ObNotificationService} from '@oblique/oblique';
 import {IUser, Role} from './shared/model/user';
 import {SignInComponent} from './shared/components/sign-in/sign-in.component';
@@ -54,8 +54,26 @@ export class AppComponent implements OnInit {
 	}
 
 	createUser(user: IUser) {
-		this.userService.createAccount(user).subscribe((userId: string) => {
-			this.notif.success('Account created successfully');
+		this.userService.createAccount(user).subscribe({
+			next: (response: UserRegistrationResponse) => {
+				this.userService.login({
+					username: user.username,
+					password: user.password,
+					roles: [],
+					organizations: user.organizations
+				}).subscribe({
+					next: (token: IToken) => {
+						this.loginUser(token);
+						this.notif.success('Registration successful');
+					},
+					error: () => {
+						this.notif.error('Login failed after registration');
+					}
+				});
+			},
+			error: () => {
+				this.notif.error('Registration failed');
+			}
 		});
 	}
 
