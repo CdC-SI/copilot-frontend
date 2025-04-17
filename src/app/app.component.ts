bc2b23ae674758d88ad0bd60516cfc9b640e68bc
Mvinciar <matt.vincia@gmail.com>
2025-04-17 14:24:09 +0200

    eslint

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,14 +1,14 @@
 import {Component, OnInit} from '@angular/core';
 import {MatDialog} from '@angular/material/dialog';
 import {SignUpComponent} from './shared/components/sign-up/sign-up.component';
-import {UserService, UserRegistrationResponse} from './shared/services/user.service';
+import {UserRegistrationResponse, UserService} from './shared/services/user.service';
 import {ObINavigationLink, ObNotificationService} from '@oblique/oblique';
-import {IUser, Role} from './shared/model/user';
+import {IUser} from './shared/model/user';
 import {SignInComponent} from './shared/components/sign-in/sign-in.component';
 import {TokenService} from './shared/services/token.service';
 import {IToken} from './shared/model/token';
-import { SettingsService } from './shared/services/settings.service';
-import { SettingsType } from './shared/model/settings';
+import {SettingsService} from './shared/services/settings.service';
+import {SettingsType} from './shared/model/settings';
 
 @Component({
 	selector: 'zco-root',
@@ -33,20 +33,20 @@ export class AppComponent implements OnInit {
 	) {}
 
 	ngOnInit() {
-		this.settingsService.getSettings(SettingsType.PROJECT_VERSION).subscribe(
-			version => this.projectVersion = version[0]
-		);
+		this.settingsService.getSettings(SettingsType.PROJECT_VERSION).subscribe(version => (this.projectVersion = version[0]));
 	}
 
 	getNavigation() {
 		return this.userService.isAuthenticatedAsAdmin() ? this.navigationAdmin : this.navigation;
 	}
+
 	openSignUpDialog() {
 		this.dialog
 			.open(SignUpComponent, {width: '800px'})
 			.afterClosed()
 			.subscribe(result => result && this.createUser(result));
 	}
+
 	openSignInDialog() {
 		this.dialog
 			.open(SignInComponent, {width: '800px'})
@@ -57,20 +57,22 @@ export class AppComponent implements OnInit {
 	createUser(user: IUser) {
 		this.userService.createAccount(user).subscribe({
 			next: (response: UserRegistrationResponse) => {
-				this.userService.login({
-					username: user.username,
-					password: user.password,
-					roles: [],
-					organizations: user.organizations
-				}).subscribe({
-					next: (token: IToken) => {
-						this.loginUser(token);
-						this.notif.success('Registration successful');
-					},
-					error: () => {
-						this.notif.error('Login failed after registration');
-					}
-				});
+				this.userService
+					.login({
+						username: user.username,
+						password: user.password,
+						roles: [],
+						organizations: user.organizations
+					})
+					.subscribe({
+						next: (token: IToken) => {
+							this.loginUser(token);
+							this.notif.success('Registration successful');
+						},
+						error: () => {
+							this.notif.error('Login failed after registration');
+						}
+					});
 			},
 			error: () => {
 				this.notif.error('Registration failed');
