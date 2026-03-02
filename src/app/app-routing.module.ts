fc0db1534102777dcf60091d655896ba76ab71f7
Mvinciar <matt.vincia@gmail.com>
2026-03-02 14:02:53 +0100

    feat: implement user role-based navigation and document acceptance flow in sign-up

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,11 +6,12 @@ import {TipsComponent} from './tips/tips.component';
 import {AdminGuard} from './shared/services/admin.guard';
 import {ToolsHomeComponent} from './tools/tools-home/tools-home.component';
 import {PersonalDocumentsComponent} from './personal-documents/personal-documents.component';
+import {UserGuard} from './shared/services/user.guard';
 
 const routes: Routes = [
 	{path: '', redirectTo: 'chat', pathMatch: 'full'},
 	{path: 'chat', component: ChatComponent},
-	{path: 'personal-documents', component: PersonalDocumentsComponent},
+	{path: 'personal-documents', component: PersonalDocumentsComponent, canActivate: [UserGuard]},
 	{path: 'admin', component: AdminComponent, canActivate: [AdminGuard]},
 	{path: 'tips', component: TipsComponent},
 	{path: 'tools', component: ToolsHomeComponent}
